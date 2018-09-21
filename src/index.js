b07e8f914aa4355f381b2fe9d4cb55833c9a9663
Horoshaya <daria.shubenok@gmail.com>
2018-09-21 16:50:00 +0300

    not yet

--- src/index.js
+++ src/index.js
@@ -1,43 +1,85 @@
 module.exports = function solveSudoku(matrix) {
-	function solveSudoku(matrix) {
+function solveSudoku(matrix) {
 	var rows;
 	var number;
-	var add = [1,2,3,4,5,6,7,8,9];
-	var zeroIndecec = [];
+	var columns = [];
+	//var allNumbers = [1,2,3,4,5,6,7,8,9];
+	var indececRow = [];
+	var indececColumn = [];
+	var addRow = [];
+	var addColumn = [];
 	for (var i = 0; matrix.length > i; i++) {
-	    rows = matrix[i];
-	    for (var h = 0; h <=8; h++) {
+		rows = matrix[i];
+		//add[i] = [];
+		var allNumbers = [1,2,3,4,5,6,7,8,9];
+		for (var h = 0; h <=8; h++) {
+			for (var k = 0; k <=8; k++) {
+				if (rows[h] == 0) {
+					indececRow.push(h);
+					break;
+				}
 
-	       for (var k = 0; k <=8; k++) {
-	       	if (rows[h] == 0) {
-				zeroIndecec.push(h);
-				break;
-	          }
-	          if (rows[h] == add[k]) {
-	          	add.splice(k, 1);
-	          	--k;
+				if (rows[h] == allNumbers[k]) {
+					allNumbers.splice(k, 1);
+					--k;
+				}
+			}
+		}
 
-	          } 
-	           
-	       }
+		addRow.push(allNumbers);
+
+		for (var y = 0; indececRow.length > y; y++) {
+			
+			index = indececRow[y];
+
+			for (var j = 0; matrix.length > j; j++) {
+				column = matrix[j];
+				columns = column[index];
+			}
+
+			var allNumbers = [1,2,3,4,5,6,7,8,9];
+			for (var h = 0; h <=8; h++) {
+				for (var k = 0; k <=9; k++) {
+					if (columns[h] == 0) {
+						indececColumn.push(h);
+						break;
+					}
+
+					if (columns[h] == allNumbers[k]) {
+						allNumbers.splice(k, 1);
+						--k;
+					}
+				}
+			}
+
+			addColumn.push(allNumbers);
+
+			for (var m = 0; matrix.length > m; m++) { 
+				// for(var r = 0; matrix.length > r; r++) {
+					addRow = addRow[m];
+					addColumn = addColumn[m];
+					if (addRow.length == m || addColumn.length  == m) {
+						break;
+					}
+
+					if (addRow[m] == addColumn[m]) {
+						var thisNumber = addRow[m];
+						matrix[m].splice(index, 1, thisNumber);
+						break;
+					}
+				// }
+			}
+		}	
+			
+		
+
+		//console.log(add);
+		//console.log(zeroIndecec);
+			//break
 	}
-	console.log(add);
-	console.log(zeroIndecec);
-	break
-	 }     
-	    for (var j = 0; matrix.length > j; j++) {
-	        number = rows[j];
-	        
-	            
-	        if (number == 0) {
 
-	        }
-	    
-	}
-
-		console.log(rows);
-
-
+		console.log(matrix);
+		//return (add);
 }
 
 
@@ -52,7 +94,7 @@ solveSudoku([
     [2, 8, 7, 4, 1, 9, 6, 3, 5],
     [3, 4, 5, 2, 8, 6, 1, 7, 9]
   ]);
-	
+
 	}
 
 
